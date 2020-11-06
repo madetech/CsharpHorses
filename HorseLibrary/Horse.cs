64e831a572746746b3c3733dc233d732805eedbe
Emma Blott <blott23@gmail.com>
2020-11-06 17:10:01 +0000

    first commit

---
+++ HorseLibrary/Horse.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace HorseLibrary
+{
+    public interface IHorseSound
+    {
+        public string Speak();
+    }
+
+    public class HorseSound : IHorseSound
+    {
+        public string Speak()
+        {
+            return "neeeeeeeiiiiiigggghhhhhhh";
+        }
+    }
+
+    public class RobotHorseSound : IHorseSound
+    {
+        public string Speak()
+        {
+            return "100101010001010110101001";
+        }
+    }
+
+    public class Horse 
+    {
+        public bool Tail { get; set; }
+        public IHorseSound Sound { get; set; }
+        
+        public Horse(bool tail, IHorseSound sound)
+        {
+            this.Tail = tail;
+            Sound = sound;
+        }
+        
+
+        
+
+        public string Speak()
+        {
+            return Sound.Speak();
+        }
+        
+    }
+}
---
+++ HorseLibraryTest/HorseLibraryTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using FluentAssertions;
+using HorseLibrary;
+using NUnit.Framework;
+
+namespace HorseLibraryTest
+{
+    [TestFixture]
+    class LibraryTest
+    {
+        [Test]
+        public void HorseHasTail()
+        {
+            var horseSound = new HorseSound();
+            var myLittlePony = new Horse(true,horseSound);
+            myLittlePony.Tail.Should().BeTrue();
+        }
+        [Test]
+        public void HorseCanNeigh()
+        {
+            var horseSound = new HorseSound();
+            var myLittlePony = new Horse(true,horseSound);
+            myLittlePony.Speak().Should().Be("neeeeeeeiiiiiigggghhhhhhh");
+        }
+        [Test]
+        public void RobotHorseCanBinary()
+        {
+            var horseSound = new RobotHorseSound();
+            var myLittlePony = new Horse(true,horseSound);
+            myLittlePony.Speak().Should().Be("100101010001010110101001");
+        }
+        
+        
+    }
+}
